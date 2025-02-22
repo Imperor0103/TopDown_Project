7182b0edf529a8ae698f1ce926e9779889c13a9f
Imperor <imperor0103@gmail.com>
2025-02-23 01:29:51 +0900

    [Feat] 유니티 Input System을 적용

--- Assets/Scripts/Entity/PlayerController.cs
+++ Assets/Scripts/Entity/PlayerController.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
 
 public class PlayerController : BaseController
 {
@@ -20,13 +22,60 @@ public class PlayerController : BaseController
         camera = Camera.main;
     }
 
+    /// <summary>
+    /// 유니티 Input System을 적용하기 전의 Move, Look, Fire 
+    /// </summary>
     protected override void HandleAction()
     {
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
-        movementDirection = new Vector2(horizontal, vertical).normalized;
+        /// OnMove로 이동
+        //float horizontal = Input.GetAxisRaw("Horizontal");
+        //float vertical = Input.GetAxisRaw("Vertical");
+        //movementDirection = new Vector2(horizontal, vertical).normalized;
 
-        Vector2 mousePosition = Input.mousePosition;
+        /// OnLook으로 이동
+        //Vector2 mousePosition = Input.mousePosition;
+        //Vector2 worldPos = camera.ScreenToWorldPoint(mousePosition);
+        //lookDirection = (worldPos - (Vector2)transform.position);
+
+        //if (lookDirection.magnitude < .9f)
+        //{
+        //    lookDirection = Vector2.zero;
+        //}
+        //else
+        //{
+        //    lookDirection = lookDirection.normalized;
+        //}
+
+        /// OnFire로 이동
+        // BaseController(부모 클래스)에서 사용할 isAttacking 변수 값 설정
+        //isAttacking = Input.GetMouseButton(0);
+    }
+
+    public override void Death()
+    {
+        base.Death();
+        gameManager.GameOver();
+    }
+
+
+    /// <summary>
+    /// 유니티의 Input System을 적용
+    /// OnMove, OnLOok, OnFire
+    /// 
+    /// 기존의 입력을 사용하지 않고
+    /// InputValue을 매개변수로 전달하여, 유니티에서 제공한 InputValue를 사용한다
+    /// 
+    /// 각 키가 입력되면 위의 세 함수가 호출되어 처리한다
+    /// </summary>
+    /// <param name="inputValue"></param>
+    void OnMove(InputValue inputValue)
+    {
+        movementDirection = inputValue.Get<Vector2>();
+        movementDirection = movementDirection.normalized;
+    }
+    void OnLook(InputValue inputValue)
+    {
+        Vector2 mousePosition = inputValue.Get<Vector2>();
         Vector2 worldPos = camera.ScreenToWorldPoint(mousePosition);
         lookDirection = (worldPos - (Vector2)transform.position);
 
@@ -38,15 +87,15 @@ public class PlayerController : BaseController
         {
             lookDirection = lookDirection.normalized;
         }
-
-        // BaseController(부모 클래스)에서 사용할 isAttacking 변수 값 설정
-        isAttacking = Input.GetMouseButton(0);
     }
-
-    public override void Death()
+    void OnFire(InputValue inputValue)
     {
-        base.Death();
-        gameManager.GameOver();
-    }
+        // EventSystem은 Canvas 추가하면 자동생성되는 그 EventSystem이다
+        // UI를 클릭했는지 판단한다
+        // 여기서는 UI를 클릭하면 공격하지 않게 하기 위해 추가
+        if (EventSystem.current.IsPointerOverGameObject())  
+            return;
 
+        isAttacking = inputValue.isPressed;
+    }
 }
